6a22b055061cd8df567236285bf3d217ac424ab8
AntonioEduardo-Dev <antonio.eduardo.dev@gmail.com>
2024-09-21 11:58:15 -0300

    feat: make command ImportWords

---
+++ backend/app/Console/Commands/ImportWords.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\WordService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class ImportWords extends Command
+{
+    protected $signature = 'importar:palavras {url}';
+    protected $description = 'Baixa uma lista de palavras e importa para o banco de dados';
+
+    public function __construct(private WordService $wordService)
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $url = $this->argument('url');
+        $response = Http::get($url);
+
+        if ($response->successful()) {
+            $listWords = explode("\n", $response->body());
+
+            // Usar o service para importar as palavras
+            $this->wordService->importWords($listWords);
+            $this->info('Palavras importadas com sucesso!');
+        } else {
+            $this->error('Falha ao baixar a lista de palavras.');
+        }
+    }
+}
