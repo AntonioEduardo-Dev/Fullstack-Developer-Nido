3496367a52032f036b30ab38fa0819d2aa672356
AntonioEduardo-Dev <antonio.eduardo.dev@gmail.com>
2024-09-21 11:48:11 -0300

    feat: inject WordRepositoryInterface in WordRepository

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\Api\WordRepositoryInterface;
+use App\Repositories\Api\WordRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(WordRepositoryInterface::class, WordRepository::class);
     }
 
     /**
--- backend/app/Services/WordService.php
+++ backend/app/Services/WordService.php
@@ -3,7 +3,6 @@
 namespace App\Services;
 
 use App\Repositories\Api\WordRepository;
-use Illuminate\Support\Facades\Log;
 
 class WordService
 {
