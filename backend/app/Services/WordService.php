845be01ecadf0ab3bb0120c21bfd07f9a0a45d1f
AntonioEduardo-Dev <antonio.eduardo.dev@gmail.com>
2024-09-21 11:47:35 -0300

    feat: make WordService to import words

---
+++ backend/app/Services/WordService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Api\WordRepository;
+use Illuminate\Support\Facades\Log;
+
+class WordService
+{
+    public function __construct(protected WordRepository $wordRepository)
+    {}
+
+    public function importWords(array $listWords)
+    {
+        foreach ($listWords as $word) {
+            $word = trim($word);
+            if (!!$word && !empty($word)) {
+                 $this->wordRepository->saveInBatch(['word' => $word]);
+            }
+        }
+    }
+}
