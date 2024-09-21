f74d0ff765be691352c5f27ce583bd9ee546ee7c
AntonioEduardo-Dev <antonio.eduardo.dev@gmail.com>
2024-09-21 16:09:28 -0300

    refactor: change dicionaryApiService to cURLRepository

--- backend/app/Services/Api/DicionaryApiService.php
+++ backend/app/Services/Api/DicionaryApiService.php
@@ -2,15 +2,15 @@
 
 namespace App\Services\Api;
 
-use App\Repositories\Api\GuzzleHttpRepository;
+use App\Repositories\cURLRepository;
 
 class DicionaryApiService
 {
-    public function __construct(protected GuzzleHttpRepository $guzzleHttpRepository)
+    public function __construct(protected cURLRepository $cURLRepository)
     {}
 
     public function getAllItems($word)
     {
-        return $this->guzzleHttpRepository->getAllItems('GET', "https://api.dictionaryapi.dev/api/v2/entries/en/{$word}");
+        return $this->cURLRepository->getAllItems('GET', "https://api.dictionaryapi.dev/api/v2/entries/en/{$word}");
     }
 }
