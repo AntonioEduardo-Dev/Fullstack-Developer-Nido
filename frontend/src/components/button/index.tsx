417db4b2449f0f2245fb2b7f42c7b55ee8f6897f
AntonioEduardo-Dev <antonio.eduardo.dev@gmail.com>
2024-09-22 16:52:35 -0300

    feat: add disabled attr in button

--- frontend/src/components/button/index.tsx
+++ frontend/src/components/button/index.tsx
@@ -1,12 +1,13 @@
 import { ButtonProps } from "../../interface/buttonInterface";
 
-const Button: React.FC<ButtonProps> = ({ title, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ title, onClick, disabled }) => {
   return (
     <div className="relative mb-6 w-[80%]">
       <button
         type="submit"
         className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 "
-        onClick={onClick}>
+        onClick={onClick}
+        disabled={disabled}>
         {title}
       </button>
     </div>
--- frontend/src/interface/buttonInterface.tsx
+++ frontend/src/interface/buttonInterface.tsx
@@ -1,4 +1,5 @@
 export interface ButtonProps {
   title: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
