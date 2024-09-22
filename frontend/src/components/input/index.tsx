ac15555ccb883b55432e3129d4a424fff601c9e5
AntonioEduardo-Dev <antonio.eduardo.dev@gmail.com>
2024-09-22 13:25:51 -0300

    feat: make component input

---
+++ frontend/src/components/input/index.tsx
@@ -0,0 +1,19 @@
+import { InputProps } from "../../interface/inputInterface";
+
+const Input: React.FC<InputProps> = ({ id, type, placeholder, icon }) => {
+  return (
+    <div className="relative mb-6 w-[80%] ">
+      <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+        {icon}
+      </div>
+      <input
+        type={type}
+        id={id}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 selection:border-red-500 block w-full ps-10 p-2.5"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default Input;
