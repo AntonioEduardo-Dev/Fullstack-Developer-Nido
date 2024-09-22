af056d071a33dda0b6af15ea30e9b390b3102214
AntonioEduardo-Dev <antonio.eduardo.dev@gmail.com>
2024-09-22 14:12:18 -0300

    feat: add logout in nav

--- frontend/src/components/nav/index.tsx
+++ frontend/src/components/nav/index.tsx
@@ -1,22 +1,36 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { NavProps } from "../../interface/navInterface";
 import { IoMenu } from "react-icons/io5";
 import { IoClose } from "react-icons/io5";
 import { IoBookmarks } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Nav: React.FC<NavProps> = () => {
+  const navigate = useNavigate();
+  const { logout } = useContext(AuthContext);
   const [menuClass, setMenuClass] = useState("");
   const [iconName, setIconName] = useState("menu");
+  const token = localStorage.getItem('authToken');
 
   const toggleMenu = () => {
     setIconName(iconName === "menu" ? "close" : "menu");
     setMenuClass(menuClass ? "" : "top-[7%]");
   };
 
+  const logOut = () => {
+    logout();
+    navigate("/entrar"); // Redireciona para a home após login bem-sucedido
+  };
+
+  const login = () => {
+    navigate("/entrar"); // Redireciona para a home após login bem-sucedido
+  };
+
   return (
     <nav className="flex justify-between items-center w-[92%] mx-auto py-3">
       <div>
-        <IoBookmarks size={36} color="#fbc2eb" />
+        <IoBookmarks className="cursor-pointer" size={36} color="#fbc2eb" />
       </div>
       <div
         className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[100vh] left-0 top-[-100%] md:w-auto z-10 w-full flex items-center px-5 ${menuClass}`}>
@@ -26,16 +40,12 @@ const Nav: React.FC<NavProps> = () => {
               Dicionario
             </a>
           </li>
-          <li>
-            <a className="hover:text-gray-500" href="#">
-              Favoritos
-            </a>
-          </li>
         </ul>
       </div>
       <div className="flex items-center gap-6">
-        <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">
-          Login
+        <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]"
+            onClick={ !!token ? logOut : login}>
+          { !!token ? "Sair" : "Entrar"}
         </button>
         <div>
           {iconName === "menu" ? (
