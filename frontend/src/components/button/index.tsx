ade7cf95a3e54b4b003b6e1f4dbca537300bb1fc
AntonioEduardo-Dev <antonio.eduardo.dev@gmail.com>
2024-09-22 14:11:08 -0300

    refactor: change input and button to accept functions

--- frontend/src/components/button/index.tsx
+++ frontend/src/components/button/index.tsx
@@ -1,11 +1,12 @@
 import { ButtonProps } from "../../interface/buttonInterface";
 
-const Button: React.FC<ButtonProps> = ({ title }) => {
+const Button: React.FC<ButtonProps> = ({ title, onClick }) => {
   return (
     <div className="relative mb-6 w-[80%]">
       <button
         type="submit"
-        className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">
+        className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 "
+        onClick={onClick}>
         {title}
       </button>
     </div>
